bc0eee3c69a2799f1feab5278754d6a8cf1b5fa5
Jason <nocvi111@gmail.com>
2022-09-26 11:32:41 +0800

    feat: db connection configs

---
+++ configs/configs.go
@@ -0,0 +1,48 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+type server struct {
+	Host string
+	Port string
+}
+
+type database struct {
+	Dialect  string
+	Username string
+	Password string
+	Protocol string
+	Host     string
+	Name     string
+	Options  string
+}
+
+var Server = server{}
+var Database = database{}
+
+func init() {
+	cfg, err := ini.Load("configs.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	Server = server{
+		Host: cfg.Section("server").Key("host").String(),
+		Port: cfg.Section("server").Key("port").String(),
+	}
+	Database = database{
+		Dialect:  cfg.Section("database").Key("dialect").String(),
+		Username: cfg.Section("database").Key("username").String(),
+		Password: cfg.Section("database").Key("password").String(),
+		Protocol: cfg.Section("database").Key("protocol").String(),
+		Host:     cfg.Section("database").Key("host").String(),
+		Name:     cfg.Section("database").Key("name").String(),
+		Options:  cfg.Section("database").Key("options").String(),
+	}
+}

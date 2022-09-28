274b41c8c99de08320b4c9855fcd17b3ffd58938
Jason <nocvi111@gmail.com>
2022-09-28 20:42:14 +0800

    chore: server host and port

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shlason/url-shortener/configs"
 	"github.com/shlason/url-shortener/routes"
 	"golang.org/x/crypto/acme/autocert"
 	"golang.org/x/sync/errgroup"
@@ -20,13 +22,13 @@ func main() {
 	routes.RegisteStaticContentRoutes(r)
 	routes.RegisteShortRoutes(apiRoute)
 
-	r.Run()
+	// r.Run()
 
 	g.Go(func() error {
-		return http.ListenAndServe(":http", http.RedirectHandler("https://sidesideeffect.io", http.StatusSeeOther))
+		return http.ListenAndServe(":http", http.RedirectHandler(fmt.Sprintf("https://%s", configs.Server.Host), http.StatusSeeOther))
 	})
 	g.Go(func() error {
-		return http.Serve(autocert.NewListener("sidesideeffect.io"), r)
+		return http.Serve(autocert.NewListener(configs.Server.Host), r)
 	})
 
 	if err := g.Wait(); err != nil {
